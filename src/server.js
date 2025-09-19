ae612ac1cf7f9b53123a52f47abc474a26821acb
Volodymyr <sainchukvova2104@gmail.com>
2025-09-19 23:49:25 +0300

    fix bugs

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import pino from 'pino-http';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import errrorHandler from './middlewares/errorHandler.js';
+import studentRoutes from './routers/contacts.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +13,8 @@ export default function setupServer() {
   app.use(cors());
   app.use(pino());
   app.use(express.json());
+
+  app.use('/students', studentRoutes);
 
   app.get('/', (req, res) => {
     req.log.info('GET / called');
