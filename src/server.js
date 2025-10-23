e84e6d0b8abe638cb755d68e92ffeb72ba5c2df0
Volodymyr <sainchukvova2104@gmail.com>
2025-10-23 21:42:57 +0300

    fix bugs

--- src/server.js
+++ src/server.js
@@ -1,27 +1,33 @@
 import express from 'express';
 import 'dotenv/config';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import pino from 'pino-http';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import errorHandler from './middlewares/errorHandler.js';
 import router from './routers/index.js';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
 const PORT = process.env.PORT || 3000;
 
-export default function setupServer() {
-  initMongoConnection();
+export default async function setupServer() {
+  await initMongoConnection();
+
   const app = express();
 
   app.use(cors());
   app.use(pino());
   app.use(express.json());
+  app.use(cookieParser());
+
+  const swaggerDocument = YAML.load(
+    path.join(process.cwd(), 'docs/openapi.yaml'),
+  );
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use('/', router);
-
-  app.get('/', (req, res) => {
-    req.log.info('GET / called');
-    res.send('main is here');
-  });
 
   app.use((req, res) => {
     res.status(404).json({ status: 404, message: 'Route not found' });
